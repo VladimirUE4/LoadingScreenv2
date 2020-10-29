b0f970242370976eb54e1c4009e522b0423bfc52
Vlad <53918010+VladimirUE4@users.noreply.github.com>
2020-10-29 12:00:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Loading-Screen
+Un loading screen fait a la va vite.. fonctionnel
+
+
+Avec le parallax de js. (En gros ca bouge avec ta souris)
+
+Preview : https://imgur.com/TUb1BKu
+
+If you have any issues contact me : 
+DlaV#9404
---
+++ __resource.lua
@@ -0,0 +1,14 @@
+files {
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "bg.png",
+    "parallax.js",
+    "style.css",
+    "index.html",
+    "jul.mp3"
+}
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
